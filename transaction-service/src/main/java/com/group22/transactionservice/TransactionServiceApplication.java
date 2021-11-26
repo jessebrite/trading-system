973db4b8487f129710b8c54535cf169dbef9176b
jessebrite <aboagye@protonmail.com>
2021-11-26 15:34:42 +0000

    Initial commit

---
+++ transaction-service/src/main/java/com/group22/transactionservice/TransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.group22.transactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionServiceApplication.class, args);
+    }
+
+}
---
+++ transaction-service/src/test/java/com/group22/transactionservice/TransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group22.transactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
