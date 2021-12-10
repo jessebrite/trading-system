dec0eca35e6968835c74b2b27184836320b3bd4d
Selasi Ayittah <selasiayittah3@gmail.com>
2021-12-10 16:59:00 +0000

    modifited the initial_migration.sql

--- client-service/src/main/resources/db/migration/V1__initial_migration.sql
+++ client-service/src/main/resources/db/migration/V1__initial_migration.sql
@@ -70,3 +70,18 @@ CREATE TABLE IF NOT EXISTS clients
     CONSTRAINT fk_client_order FOREIGN KEY (order_id)
         REFERENCES order_table (id)
 );
+
+
+CREATE TABLE IF NOT EXISTS market_data
+(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    last_traded_price DOUBLE PRECISION NOT NULL,
+    ask_price DOUBLE PRECISION NOT NULL,
+    bid_price DOUBLE PRECISION NOT NULL,
+    buy_limit DOUBLE PRECISION NOT NULL,
+    max_price_shift DOUBLE PRECISION NOT NULL,
+    sell_limit DOUBLE PRECISION NOT NULL,
+    ticker VARCHAR,
+    timestamp BIGINT,
+    exchange VARCHAR
+);
